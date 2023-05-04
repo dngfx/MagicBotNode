03d9086312dfd8ec54632a052f776d1705063f55
dngfx <github@dong.engineer>
2023-05-04 03:33:04 +0100

    Blank config

---
+++ conf/settings.blank.js
@@ -0,0 +1,25 @@
+const config = {
+    "host": "",
+    "port": 6697,
+    "encoding": "utf8",
+    "version": "",
+
+    "nick": "",
+    "username": "",
+    "gecos": "",
+
+    // SASL
+    "account": {
+        "account": "",
+        "password": ""
+    },
+
+    "tls": {
+        "servername": "",
+        "host": "",
+        "port": 6697,
+        "rejectUnauthorized": false
+    }
+};
+
+module.exports = config;
